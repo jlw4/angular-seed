2b89aaee40e216fccc61db2a2c08036c1bfd8418
jlw4 <johnleewilson4@gmail.com>
2014-10-06 12:49:45 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var app = angular.module('App',[]);
+  
+app.controller('MainController', ['$scope', '$location', '$timeout', function($scope, $location, $timeout) {
+
+    
+	
+}]);
